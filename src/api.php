69a422a676ebe4aed34542759547b0ac3650ef0c
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2021-09-20 15:45:23 +0200

    Fix old prove redirect

--- src/api.php
+++ src/api.php
@@ -334,8 +334,8 @@ $server = new HttpServer(function (ServerRequestInterface $request){
     }
 
     if(preg_match("#^/api/redirect/prove/(?P<height>[0-9]+|.[0-9A-Za-z]+)/(?P<miner>[0-9]+|.?[0-9A-Za-z]+)$#", $request->getUri()->getPath(), $matches) > 0){
-        $b = $api->getDatabase()->getBlockByHeight($matches["height"]);
-        $miner = $api->getDatabase()->getMiner($matches["miner"]);
+        $b = $api->getDatabase()->getBlockByHeight(Utils::decodeBinaryNumber($matches["height"]));
+        $miner = $api->getDatabase()->getMiner(Utils::decodeBinaryNumber($matches["miner"]));
         if($b === null or $miner === null){
             return new Response(404, [
                 "Content-Type" => "application/json; charset=utf-8"
