d11ed04dff23973ee2c85d5178ed0e947491ddf4
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2021-09-24 14:15:41 +0200

    Implement nicer block decoder

---
+++ src/BinaryBlock.php
@@ -0,0 +1,353 @@
+<?php
+
+namespace p2pool;
+
+class BinaryBlock{
+
+    private const HASH_SIZE = 32;
+    private const DIFFICULTY_SIZE = 16;
+    private const NONCE_SIZE = 4;
+
+    private \stdClass $extra;
+
+    /**
+     * @return \stdClass
+     */
+    public function getExtra(): \stdClass {
+        return $this->extra;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMajorVersion(): int {
+        return $this->majorVersion;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMinorVersion(): int {
+        return $this->minorVersion;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimestamp(): int {
+        return $this->timestamp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMainParent(): string {
+        return $this->mainParent;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNonce(): string {
+        return $this->nonce;
+    }
+
+    /**
+     * @return string
+     */
+    public function getExtraNonce(): string {
+        return $this->extraNonce;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCoinbaseTxVersion(): int {
+        return $this->coinbaseTxVersion;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCoinbaseTxUnlockTime(): int {
+        return $this->coinbaseTxUnlockTime;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCoinbaseTxInputCount(): int {
+        return $this->coinbaseTxInputCount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCoinbaseTxInputType(): int {
+        return $this->coinbaseTxInputType;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCoinbaseTxGenHeight(): int {
+        return $this->coinbaseTxGenHeight;
+    }
+
+    /**
+     * @return object[]
+     */
+    public function getCoinbaseTxOutputs(): array {
+        return $this->coinbaseTxOutputs;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCoinbaseTxExtra(): string {
+        return $this->coinbaseTxExtra;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getTransactions(): array {
+        return $this->transactions;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublicSpendKey(): string {
+        return $this->publicSpendKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublicViewKey(): string {
+        return $this->publicViewKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCoinbaseTxPrivateKey(): string {
+        return $this->coinbaseTxPrivateKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParent(): string {
+        return $this->parent;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getUncles(): array {
+        return $this->uncles;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHeight(): int {
+        return $this->height;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDifficulty(): string {
+        return $this->difficulty;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCumulativeDifficulty(): string {
+        return $this->cumulativeDifficulty;
+    }
+
+    // MainChain data
+    private int $majorVersion;
+    private int $minorVersion;
+    private int $timestamp;
+    private string $mainParent;
+    private string $nonce;
+    private string $extraNonce;
+
+    private int $coinbaseTxVersion;
+    private int $coinbaseTxUnlockTime;
+    private int $coinbaseTxInputCount;
+    private int $coinbaseTxInputType;
+    private int $coinbaseTxGenHeight;
+    private array $coinbaseTxOutputs = [];
+    private string $coinbaseTxExtra;
+    private array $transactions = [];
+
+    // SideChain data
+    private string $id; // filled from coinbase tx
+    private string $publicSpendKey;
+    private string $publicViewKey;
+    private string $coinbaseTxPrivateKey;
+    private string $parent;
+    private array $uncles = [];
+    private int $height;
+    private string $difficulty;
+    private string $cumulativeDifficulty;
+
+
+
+    public static function fromHexDump(string $hex): BinaryBlock {
+        $index = 0;
+        $bin = hex2bin($hex);
+
+        if(strlen($bin) < 32){
+            throw new \Exception("Invalid block data");
+        }
+
+        $version = self::readUINT64($bin, $index);
+        $main = null;
+        $side = null;
+
+        $b = new BinaryBlock();
+        $b->extra = new \stdClass();
+
+        switch ($version){
+            case 1:
+                $b->extra->mainId = bin2hex(self::readBinary($bin, self::HASH_SIZE, $index));
+                $b->extra->powHash = bin2hex(self::readBinary($bin, self::HASH_SIZE, $index));
+                $b->extra->mainDifficulty = bin2hex(self::readBinary($bin, self::DIFFICULTY_SIZE, $index));
+
+                $main = self::readBinary($bin, self::readUINT64($bin, $index), $index);
+                $side = self::readBinary($bin, self::readUINT64($bin, $index), $index);
+                break;
+            case 0:
+                $main = self::readBinary($bin, self::readUINT64($bin, $index), $index);
+                $side = substr($bin, $index);
+                break;
+
+            default:
+                throw new \Exception("Unknown block version $version");
+        }
+
+        //MainChain parsing
+        $index = 0;
+
+        $b->majorVersion = self::readUINT8($main, $index);
+        $b->minorVersion = self::readUINT8($main, $index);
+        $b->timestamp = self::readVARINT($main, $index);
+        $b->mainParent = bin2hex(self::readBinary($main, self::HASH_SIZE, $index));
+        $b->nonce = bin2hex(self::readBinary($main, self::NONCE_SIZE, $index));
+
+        $b->coinbaseTxVersion = self::readUINT8($main, $index);
+        $b->coinbaseTxUnlockTime = self::readVARINT($main, $index);
+        $b->coinbaseTxInputCount = self::readUINT8($main, $index);
+        $b->coinbaseTxInputType = self::readUINT8($main, $index);
+        $b->coinbaseTxGenHeight = self::readVARINT($main, $index);
+
+        $outputCount = self::readVARINT($main, $index);
+        for($i = 0; $i < $outputCount; ++$i){
+            $reward = self::readVARINT($main, $index);
+            $k = self::readUINT8($main, $index); //TODO: EXPECT TXOUT_TO_KEY
+            $ephPublicKey = bin2hex(self::readBinary($main, self::HASH_SIZE, $index));
+            $b->coinbaseTxOutputs[$i] = (object) [
+                "index" => $i,
+                "ephemeralPublicKey" => $ephPublicKey,
+                "reward" => $reward
+            ];
+        }
+
+        $txExtra = self::readBinary($main, self::readVARINT($main, $index), $index);
+        $b->coinbaseTxExtra = bin2hex($txExtra);
+        $txExtraNULL = self::readUINT8($main, $index); //????
+
+        $txCount = self::readVARINT($main, $index);
+        for($i = 0; $i < $txCount; ++$i){
+            $b->transactions[$i] = bin2hex(self::readBinary($main, self::HASH_SIZE, $index));
+        }
+
+        //TxExtra parsing
+
+        $index = 0;
+        self::readUINT8($txExtra, $index); //TODO: expect TX_EXTRA_TAG_PUBKEY
+        $txPubKey = bin2hex(self::readBinary($txExtra, self::HASH_SIZE, $index));
+        self::readUINT8($txExtra, $index); //TODO: expect TX_EXTRA_NONCE
+        $extraNonceSize = self::readVARINT($txExtra, $index);
+        $b->extraNonce = bin2hex(self::readBinary($txExtra, self::NONCE_SIZE, $index));
+        for($i = self::NONCE_SIZE; $i < $extraNonceSize; ++$i){
+            //TODO EXPECT
+            $index++;
+        }
+
+
+        self::readUINT8($txExtra, $index); //TODO: expect TX_EXTRA_MERGE_MINING_TAG
+        self::readUINT8($txExtra, $index); //TODO: expect HASH_SIZE
+        $b->id = bin2hex(self::readBinary($txExtra, self::HASH_SIZE, $index));
+
+
+        //Side Chain parsing
+
+        $index = 0;
+
+        $b->publicSpendKey = bin2hex(self::readBinary($side, self::HASH_SIZE, $index));
+        $b->publicViewKey = bin2hex(self::readBinary($side, self::HASH_SIZE, $index));
+        $b->coinbaseTxPrivateKey = bin2hex(self::readBinary($side, self::HASH_SIZE, $index));
+        $b->parent = bin2hex(self::readBinary($side, self::HASH_SIZE, $index));
+
+        $uncleCount = self::readVARINT($side, $index);
+        for($i = 0; $i < $uncleCount; ++$i){
+            $b->uncles[$i] = bin2hex(self::readBinary($side, self::HASH_SIZE, $index));
+        }
+
+        $b->height = self::readVARINT($side, $index);
+
+        $lo = self::readVARINT($side, $index);
+        $hi = self::readVARINT($side, $index);
+        $b->difficulty = bin2hex(pack("J*", $hi, $lo));
+
+        $lo = self::readVARINT($side, $index);
+        $hi = self::readVARINT($side, $index);
+        $b->cumulativeDifficulty = bin2hex(pack("J*", $hi, $lo));
+
+
+        return $b;
+    }
+
+    private static function readUINT8(string $b, int &$index) : int {
+        return ord(self::readBinary($b, 1, $index));
+    }
+    private static function readUINT64(string $b, int &$index) : int {
+        return unpack("J", self::readBinary($b, 8, $index))[1] ?? 0;
+    }
+
+    private static function readBinary(string $b, int $len, int &$index) : string {
+        if(strlen($b) < $index + $len){
+            throw new \Exception("Reached end of data at $index for read of $len bytes");
+        }
+        return substr($b, ($index += $len) - $len, $len);
+    }
+
+    private static function readVARINT(string $d, int &$index) : int {
+        $v = 0;
+        $k = 0;
+
+        do{
+            $b = self::readUINT8($d, $index);
+            $v |= ($b & 0x7f) << $k;
+            $k += 7;
+        }while(isset($d[$index]) and ($b & 0x80));
+
+        return $v;
+    }
+}
--- tools/decode_hex_block.php
+++ tools/decode_hex_block.php
@@ -7,95 +7,64 @@ use MoneroIntegrations\MoneroPhp\Cryptonote;
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../src/constants.php";
 
-$block = hex2bin(trim($argv[1] === "-" ? stream_get_contents(STDIN) : file_get_contents($argv[1])));
+$hex = trim($argv[1] === "-" ? stream_get_contents(STDIN) : file_get_contents($argv[1]));
 
 
-function readUINT8(string $b, int &$index) : int {
-    return ord(readBinary($b, 1, $index));
-}
-function readUINT64(string $b, int &$index) : int {
-    return unpack("J", readBinary($b, 8, $index))[1] ?? 0;
-}
-
-function readBinary(string $b, int $len, int &$index) : string {
-    return substr($b, ($index += $len) - $len, $len);
-}
-
-function readVARINT(string $d, int &$index) : int {
-    $v = 0;
-    $k = 0;
-
-    do{
-        $b = ord($d[$index++]);
-        $v |= ($b & 0x7f) << $k;
-        $k += 7;
-    }while(isset($d[$index]) and ($b & 0x80));
-
-    return $v;
-}
-
-$index = 0;
-
-$version = readUINT64($block, $index);
-$mainDataLength = readUINT64($block, $index);
-
-$mainData = substr($block, $index, $mainDataLength);
-$index += $mainDataLength;
-$sideData = substr($block, $index);
-
+$b = BinaryBlock::fromHexDump($hex);
 
 $cn = new Cryptonote();
 
+echo "id: ".$b->getId()."\n";
+echo "mainId: ".($b->getExtra()->mainId ?? "none")."\n";
+echo "PoW hash: ".($b->getExtra()->powHash ?? "none")."\n";
+echo "main diff: ".($b->getExtra()->mainDifficulty ?? "none")."\n";
+
 echo "\n\n=== MainChain data blob ===\n";
 $mIndex = 0;
-echo "majorVersion: ".readUINT8($mainData, $mIndex)."\n";
-echo "minorVersion: ".readUINT8($mainData, $mIndex)."\n";
-echo "timestamp: ".readVARINT($mainData, $mIndex)."\n";
-echo "Parent: ".bin2hex(readBinary($mainData, 32, $mIndex))."\n";
-echo "Nonce: ".bin2hex(readBinary($mainData, 4, $mIndex))."\n";
+echo "majorVersion: ".$b->getMajorVersion()."\n";
+echo "minorVersion: ".$b->getMinorVersion()."\n";
+echo "timestamp: ".$b->getTimestamp()."\n";
+echo "Parent: ".$b->getMainParent()."\n";
+echo "Nonce: ".$b->getNonce()."\n";
+echo "ExtraNonce: ".$b->getExtraNonce()."\n";
 
-echo "TxVersion: ".readUINT8($mainData, $mIndex)."\n";
-echo "TxUnlock: ".readVARINT($mainData, $mIndex)."\n";
-echo "Tx???: ".readUINT8($mainData, $mIndex)."\n";
-echo "TxType(Gen=0xFF): ".readUINT8($mainData, $mIndex)."\n";
-echo "TxGenHeight: ".readVARINT($mainData, $mIndex)."\n";
+echo "TxVersion: ".$b->getCoinbaseTxVersion()."\n";
+echo "TxUnlock: ".$b->getCoinbaseTxUnlockTime()."\n";
+echo "TxInputCount: ".$b->getCoinbaseTxInputCount()."\n";
+echo "TxInputType(Gen=0xFF): ".$b->getCoinbaseTxInputType()."\n";
+echo "TxGenHeight: ".$b->getCoinbaseTxGenHeight()."\n";
 
-$outputCount = readVARINT($mainData, $mIndex);
-echo "TxOutputCount: $outputCount\n";
-for($i = 0; $i < $outputCount; ++$i){
-    $reward = readVARINT($mainData, $mIndex);
-    $k = readUINT8($mainData, $mIndex);
-    $ephPublicKey = bin2hex(readBinary($mainData, 32, $mIndex));
-    echo "TxOutput[$i]: $reward $k $ephPublicKey\n";
+
+echo "TxOutputCount: ".count($b->getCoinbaseTxOutputs())."\n";
+foreach ($b->getCoinbaseTxOutputs() as $i => $o){
+    $reward = $o->reward;
+    $ephPublicKey = $o->ephemeralPublicKey;
+    echo "TxOutput[$i]: $reward $ephPublicKey\n";
 }
-echo "TxExtra: ".bin2hex(readBinary($mainData, readVARINT($mainData, $mIndex), $mIndex))."\n";
-echo "TxExtraNULL: ".readUINT8($mainData, $mIndex)."\n";
-$txCount = readVARINT($mainData, $mIndex);
-echo "TxCount: $txCount\n";
-for($i = 0; $i < $txCount; ++$i){
-    echo "TxHash[$i]: ".bin2hex(readBinary($mainData, 32, $mIndex))."\n";
+echo "TxExtra: ".$b->getCoinbaseTxExtra()."\n";
+
+echo "TxCount: ".count($b->getTransactions())."\n";
+foreach ($b->getTransactions() as $i => $tx){
+    echo "TxHash[$i]: $tx\n";
 }
 
 echo "\n\n=== SideChain data blob ===\n";
 
 $sIndex = 0;
-$publicSpendKey = bin2hex(readBinary($sideData, 32, $sIndex));
-$publicViewKey = bin2hex(readBinary($sideData, 32, $sIndex));
+$publicSpendKey = $b->getPublicSpendKey();
+$publicViewKey = $b->getPublicViewKey();
 echo "Miner pSpend: $publicSpendKey\n";
 echo "Miner pView: $publicViewKey\n";
 echo "Miner address: " . $cn->encode_address($publicSpendKey, $publicViewKey) . "\n\n";
 
-$txPrivKey = bin2hex(readBinary($sideData, 32, $sIndex));
+$txPrivKey = $b->getCoinbaseTxPrivateKey();
 echo "Coinbase privkey: $txPrivKey\n";
-echo "SideChain Parent: ".bin2hex(readBinary($sideData, 32, $sIndex))."\n";
+echo "SideChain Parent: ".$b->getParent()."\n";
 
-$uncleCount = readVARINT($sideData, $sIndex);
-for($i = 0; $i < $uncleCount; ++$i){
-    echo "Uncle[$i]: " .bin2hex(readBinary($sideData, 32, $sIndex)) . "\n";
+foreach ($b->getUncles() as $i => $u){
+    echo "Uncle[$i]: " .$u . "\n";
 }
 
-echo "SideChain height: ". readVARINT($sideData, $sIndex) . "\n";
-echo "SideChain difficulty.lo: ". readVARINT($sideData, $sIndex) . "\n";
-echo "SideChain difficulty.hi: ". readVARINT($sideData, $sIndex) . "\n";
-echo "SideChain cumDifficulty.lo: ". readVARINT($sideData, $sIndex) . "\n";
-echo "SideChain cumDifficulty.hi: ". readVARINT($sideData, $sIndex) . "\n";
+echo "SideChain height: ". $b->getHeight() . "\n";
+echo "SideChain difficulty.: ". $b->getDifficulty() . "\n";
+echo "SideChain cumDifficulty: ". $b->getCumulativeDifficulty() . "\n";
